ff374cdcd40d70d74867a6089eca56e3e531fe2a
illusionsTime <1016843502@qq.com>
2020-07-24 21:05:22 +0800

    add view and static

---
+++ controllers/base.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+// 约定：如果子controller 存在NestPrepare()方法，就实现了该接口，
+type NestPreparer interface {
+	NestPrepare()
+}
+type BaseController struct {
+	beego.Controller
+}
+
+func (ctx *BaseController) Prepare() {
+	// 将页面路径 保存到 Path变量里面
+	ctx.Data["Path"] = ctx.Ctx.Request.RequestURI
+	if app, ok := ctx.AppController.(NestPreparer); ok {
+		app.NestPrepare()
+	}
+}
--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"Yukino/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Include(&controllers.IndexController{})
+	beego.ErrorController(&controllers.ErrorController{})
 }
